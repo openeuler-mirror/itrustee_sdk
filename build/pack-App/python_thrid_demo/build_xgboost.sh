8e99d2ccbb0c08f4e74cccad33a48a2601e9c056
王晓 <438948460@qq.com>
2023-07-13 03:14:59 +0000

    update build/pack-App/python_thrid_demo/build_xgboost.sh.
    Signed-off-by: 王晓 <438948460@qq.com>

--- build/pack-App/python_thrid_demo/build_xgboost.sh
+++ build/pack-App/python_thrid_demo/build_xgboost.sh
@@ -1,4 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Description: preare toolchains and env for build ta.
+# Copyright @ Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+# Licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan
+# PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 set -e
 
 SDKDIR=$1
